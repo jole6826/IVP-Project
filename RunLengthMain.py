13952072100848e47bc4982d1bdbcb7760d3e027
ManuPf <35135829+ManuPf@users.noreply.github.com>
2018-01-06 19:32:47 +0100

    Test.jpg in imgs Ordenr verschoben; RunLengthMain: hinzufügen von Funktionen zum Einlesen aller Dateien mit .jpg und Abfragen ob Bild grau oder bunt

--- RunLengthMain.py
+++ RunLengthMain.py
@@ -1,19 +1,29 @@
-
+import glob
 import cv2
 import numpy as np
 import pickle
+from os.path import basename
 
 from RunLengthEncoding import runLength
 
+files = glob.glob('imgs/*.jpg')
 
-pic = cv2.imread('Test.jpg')
-pickle.dump(pic, open('test.p','wb'), protocol=pickle.HIGHEST_PROTOCOL)
-b = pic[:,:,0]
-g = pic[:,:,1]
-r = pic[:,:,2]
 
-encoded_vec_B = runLength(b)
-encoded_vec_G = runLength(g)
-encoded_vec_R = runLength(r)
+for file in files
+    filename = basename(files[0])[:-4]
+    pic = cv2.imread(file)
+    pickle.dump(pic, open(filename+'.p','wb'), protocol=pickle.HIGHEST_PROTOCOL)
 
-pickle.dump([encoded_vec_B,encoded_vec_G,encoded_vec_R], open('runLengthBGR.p','wb'), protocol=pickle.HIGHEST_PROTOCOL)
+    if len(pic.shape) == 2:
+        encoded_vec = runLength(pic)
+    else:
+        b = pic[:,:,0]
+        g = pic[:,:,1]
+        r = pic[:,:,2]
+
+        encoded_vec_B = runLength(b)
+        encoded_vec_G = runLength(g)
+        encoded_vec_R = runLength(r)
+        encoded_vec = [encoded_vec_B,encoded_vec_G,encoded_vec_R]
+
+    pickle.dump(encoded_vec, open('runLength'+filename+'.p','wb'), protocol=pickle.HIGHEST_PROTOCOL)
